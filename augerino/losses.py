246a0a3f76a30419469fb8baf6915f545fa06949
Allan Zhou <8732147+AllanYangZhou@users.noreply.github.com>
2020-12-17 10:47:20 -0800

    Add val acc, compare

--- augerino/losses.py
+++ augerino/losses.py
@@ -30,3 +30,9 @@ def mlp_aug_loss(outputs, labels, model,
     aug_loss = model.aug.weights.norm()
 
     return base_loss - reg * aug_loss
+
+def no_aug_loss(outputs, labels, model,
+                  base_loss_fn=torch.nn.CrossEntropyLoss(), reg=0.01):
+    del model, reg
+    base_loss = base_loss_fn(outputs, labels)
+    return base_loss
--- experiments/rotmnist/runner.py
+++ experiments/rotmnist/runner.py
@@ -3,27 +3,37 @@ import numpy as np
 import math
 import argparse
 from augerino import datasets, models, losses
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from oil.utils.mytqdm import tqdm
 from smallnet import smallnet
 import pandas as pd
+from tqdm import tqdm
+
+
 def main(args):
     net = smallnet(in_channels=1,num_targets=10)
-    augerino = models.UniformAug()
-    model = models.AugAveragedModel(net, augerino,ncopies=args.ncopies)
-    
-    start_widths = torch.ones(6) * -5.
-    start_widths[2] = 1.
-    model.aug.set_width(start_widths)
+    if args.disable_aug:
+        model = net
+    else:
+        augerino = models.UniformAug()
+        model = models.AugAveragedModel(net, augerino,ncopies=args.ncopies)
+        start_widths = torch.ones(6) * -5.
+        start_widths[2] = 1.
+        model.aug.set_width(start_widths)
     
     softplus = torch.nn.Softplus()
     
     dataset = datasets.RotMNIST("~/datasets/", train=True)
-    trainloader = DataLoader(dataset, batch_size=args.batch_size)
+    train_dset, val_dset = random_split(dataset, (50000, 10000))
+    trainloader = DataLoader(train_dset, batch_size=args.batch_size)
+    valloader = DataLoader(val_dset, batch_size=args.batch_size)
 
-    optimizer = torch.optim.Adam([{'name': 'model', 'params': model.model.parameters(), "weight_decay": args.wd}, 
-                                  {'name': 'aug', 'params': model.aug.parameters(), "weight_decay": 0.}], 
-                                 lr=args.lr)
+    if args.disable_aug:
+        param_groups = model.parameters()
+    else:
+        param_groups = [{'name': 'model', 'params': model.model.parameters(), "weight_decay": args.wd}]
+        param_groups.append({'name': 'aug', 'params': model.aug.parameters(), "weight_decay": 0.})
+    optimizer = torch.optim.Adam(param_groups, lr=args.lr)
     use_cuda = torch.cuda.is_available()
     if use_cuda:
         model = model.cuda()
@@ -33,13 +43,14 @@ def main(args):
     fname = "/model" + str(args.aug_reg) + "_init.pt"
     torch.save(model.state_dict(), args.dir + fname)
 
-    criterion = losses.safe_unif_aug_loss
+    criterion = losses.no_aug_loss if args.disable_aug else losses.safe_unif_aug_loss
+    torch.nn.CrossEntropyLoss()
     logger = []
     for epoch in range(args.epochs):  # loop over the dataset multiple times
 
         epoch_loss = 0
         batches = 0
-        for i, data in enumerate(trainloader, 0):
+        for i, data in enumerate(tqdm(trainloader), 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
 
@@ -57,17 +68,32 @@ def main(args):
             optimizer.step()
             epoch_loss += loss.detach().item()
             batches += 1
-            print(epoch, loss.item(), softplus(model.aug.width).detach().data)
-            log = model.aug.width.tolist()
-            log += model.aug.width.grad.data.tolist()
+            log = []
+            if not args.disable_aug:
+                log += model.aug.width.tolist()
+                log += model.aug.width.grad.data.tolist()
             log += [loss.item()]
             logger.append(log)        
+        train_acc = (outputs.detach().argmax(-1) == labels).float().mean().cpu().item()
+        # print(f"Train epoch {epoch}: Loss {loss.item()}, Acc: {train_acc}, Aug widths: {softplus(model.aug.width).detach().data}")
+        print(f"Train epoch {epoch}: Loss {loss.item()}, Acc: {train_acc}.")
+        with torch.no_grad():
+            model.eval()
+            val_accs = []
+            for i, data in enumerate(valloader):
+                inputs, labels = data
+                if use_cuda:
+                    inputs, labels = inputs.cuda(), labels.cuda()
+                outputs = model(inputs)
+                acc = (outputs.argmax(-1) == labels).float().mean().cpu().item()
+                val_accs.append(acc)
+            print(f"Val accuracy: {np.mean(val_accs)}")
+            model.train()
 
     fname = "/model" + str(args.aug_reg) + ".pt"
     torch.save(model.state_dict(), args.dir + fname)
     df = pd.DataFrame(logger)
     df.to_pickle(args.dir + "/auglog_" + str(args.aug_reg) + ".pkl")
-
 
 
 if __name__ == '__main__':
@@ -124,6 +150,7 @@ if __name__ == '__main__':
         metavar="N",
         help="number of augmentations in network (defualt: 4)"
     )
+    parser.add_argument("--disable_aug", action="store_true")
     args = parser.parse_args()
 
     main(args)
